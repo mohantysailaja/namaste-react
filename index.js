e8f08ffc86bbbe03549843a3ecb8fc49e11cba7d
Sailaja Mohanty <sailajamohanty@Sailajas-MacBook-Pro.local>
2023-09-13 18:37:28 +0100

    first git commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const heading = document.createElement("h1");
+heading.innerHTML ="Hello world from javascript !";
+//append 
+const root = document.getElementById("root");
+root.appendChild(heading);
