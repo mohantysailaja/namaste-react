5fd1f1f69ffb5b9ff9011defad43a50068a4f341
Sailaja Mohanty <sailajamohanty@Sailajas-MacBook-Pro.local>
2023-09-13 19:24:27 +0100

    my first ever react code

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const heading = document.createElement("h1");
+
+//javascript code for hello world 
+/*const heading = document.createElement("h1");
 heading.innerHTML ="Hello world from javascript !";
-//append 
-const root = document.getElementById("root");
-root.appendChild(heading);
+*/
+/*<!--REACT CODE STARTS--> */
+   const heading = React.createElement("h1",{},"hello world from React");
+   const root = ReactDOM.createRoot(document.getElementById("root"));
+   root.render(heading);
