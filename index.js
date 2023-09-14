47c9b36952137a0ed95f47c964203b3becc419b7
Sailaja Mohanty <sailajamohanty@Sailajas-MacBook-Pro.local>
2023-09-14 13:59:41 +0100

    nested element added using react

--- index.js
+++
@@ -1,9 +0,0 @@
-
-//javascript code for hello world 
-/*const heading = document.createElement("h1");
-heading.innerHTML ="Hello world from javascript !";
-*/
-/*<!--REACT CODE STARTS--> */
-   const heading = React.createElement("h1",{},"hello world from React");
-   const root = ReactDOM.createRoot(document.getElementById("root"));
-   root.render(heading);
