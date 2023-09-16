c72071ff6f2aa2ba5cb80bd0ddbc1cea2185b806
Sailaja Mohanty <sailajamohanty@Sailajas-MacBook-Pro.local>
2023-09-16 22:22:58 +0100

    jsx and javascript functional componenet

--- App.js
+++ App.js
@@ -1,22 +1,66 @@
-import React from 'react';
-import ReactDOM  from 'react-dom/client';
+import React from "react";
+import ReactDOM  from "React-dom/client";
 
-//React.createElement => Obect => HTMLElement(render) 
 
-const heading =React.createElement(
-"h1",{id:"heading"},
-"Namaste React 🧨"
-);
+//React elemennt
+const elem = <span>My Span elemenet !!</span>;
+//React Element 
 
-//JSX - is not HTML ,it is HTML -like or XML - like syntax-(trnspiled before it reaches the JS)-PARCEL - BABEL 
-//JSX =>Babel transpiles it to React.craeteElement => RReactElement.js object => hTML Element(render)
-//JSX => (transpiled before it reaches the JS )-PARCEL - BABEL
-const jsxheading = (<h1  className="head" tabIndex="1">
+const jsxheading = (<h1  className="heading" tabIndex="1">
    Namaste React using jsx ☘️</h1>);
 console.log(jsxheading);
+//React elemenet inside an elemenet
+/*const title = 
+ (<h1  className="heading" tabIndex="1">
+
+   Namaste React using JSX  ☘️
+  
+   </h1>
+ 
+ );
+ */
+//console.log(jsxheading);
+
+//Titel Functional component , the </Title after where you wanna put
+const Title = () =>
+ (<h1  className="heading" tabIndex="1">
+   Namaste React using JSX  ☘️</h1>);
+console.log(jsxheading);
+
+/*const fn1 =()=>{
+   return true;
+};
+*/
+//same as 
+/*const fn2 =()=> true;*/
 
 
-//console.log(heading);
+//React Functional Component
+
+/*
+//this is good syntax with return 
+const HeadingComponent = () =>{
+   return <h1 className="heading">Namaste Recat Functional Component</h1>
+};
+*/
+//same as 
+//Component Composition(one component withing another)
+//<Title />
+//<Title></Title> are both ways and we can write multiple times
+//we can call javascript  function {Title()} like this as Title is a function at the end of the day !
+const x = 1200;
+const HeadingComponent = () =>  (
+<div id="container">
+  <Title></Title>
+  <Title />      
+   {Title()}   
+     <h1 className="heading">Namaste React Functional component 🦋</h1>
+</div>
+);
+ 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//root.render(heading);
-root.render(jsxheading);
+//root.render(jxheading);
+//rendering functional component
+root.render(<HeadingComponent />);
+
