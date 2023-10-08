2fe6270808b90b5b3f4ea9f8dd414a42dcd9b5b1
Sailaja Mohanty <sailajamohanty@Sailajas-MacBook-Pro.local>
2023-10-08 16:32:54 +0100

    new button otange icon

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -20,19 +20,32 @@ const fetchMenu = async () =>{
 };
 if(resMenuInfo === null) return <Shimmer />;
 
-    const { name } =
+    const { name, cuisines, costForTwoMessage, areaName } =
     resMenuInfo?.cards[0]?.card?.card?.info;
 
+    const{itemCards} =
+    resMenuInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    console.log(itemCards);
+     
+    const{lastMileTravelString}=
+    resMenuInfo?.cards[0]?.card?.card?.info?.sla;
+    
     return  (
         <div className="menu">
             <h1>{name}</h1>
-            <h3></h3>
+            <p>{cuisines.join(", ")} - ₹{costForTwoMessage}
+            </p>
+            <p>{areaName}{" ,"} {lastMileTravelString}
+            <button className="location-btn">
+                   <span>▾</span>
+            </button></p>
             <h2>Menu</h2>
             <ul>
-                <li>Biryani</li>
-                <li>Diet Coke</li>
-                <li>Dosa and Idlis</li>
-                <li>Icecream</li>
+                {itemCards.map((item)=> (
+                <li key={item.card.info.id}>{item.card.info.name} - {" "}
+                 ₹{item.card.info.price/100 || item.card.info.price/100}</li>
+                ))}
+               <li>Manual not dynamic</li>
             </ul>
         </div>
     );
